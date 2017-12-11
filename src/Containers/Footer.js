b1967b7a7f30d5be934970563303142ec6f067ed
Joel Mikkelsen <joelmikkelsen@Joels-iMac.local>
2017-12-10 19:23:35 -0500

    front end tweaks

--- src/Containers/Footer.js
+++ src/Containers/Footer.js
@@ -9,21 +9,26 @@ class Footer extends Component {
   }
 
   render() {
-    const footer =  <div>
+    const footer =  <div className="footer-links">
                       <a href='https://github.com/joeldmikk/songer-react' className="plain-link">SOURCE</a>&nbsp;&nbsp;\\&nbsp;&nbsp;
                       <a href='https://en.wikipedia.org/wiki/Circle_of_fifths' className="plain-link">CIRCLE OF FIFTHS</a>&nbsp;&nbsp;\\&nbsp;&nbsp;
                       <a href='http://www.joelmikkelsen.com/' className="plain-link">&copy; 2017</a>
                     </div>
 
     return (
-    <div>
-      <Panel className='footer' footer={footer}>
+    <div className='footer-wrapper'>
+      <div className='footer'>
         <div className='color-swatches-wrap'>
           <div class="green swatch"></div>SELECTED
           &nbsp;&nbsp;\\&nbsp;&nbsp;
           <div class="yellow swatch"></div>SUGGESTED
         </div>
-      </Panel>
+        <div className="footer-links">
+          <a href='https://github.com/joeldmikk/songer-react' className="plain-link">SOURCE</a>&nbsp;&nbsp;\\&nbsp;&nbsp;
+          <a href='https://en.wikipedia.org/wiki/Circle_of_fifths' className="plain-link">CIRCLE OF FIFTHS</a>&nbsp;&nbsp;\\&nbsp;&nbsp;
+          <a href='http://www.joelmikkelsen.com/' className="plain-link">&copy; 2017</a>
+        </div>
+      </div>
     </div>
     )
   }
